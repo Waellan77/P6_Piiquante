27cabd88b1257b7a40d9411053507161e4a2b053
Sylvain Perez <sylvainperez77@gmail.com>
2023-01-17 16:35:35 +0100

    add comments

--- models/User.js
+++ models/User.js
@@ -2,7 +2,8 @@
 const mongoose = require('mongoose')
 
 const uniqueValidator = require('mongoose-unique-validator')
-// user page template //
+
+// user page template for signup //
 const userSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true }
