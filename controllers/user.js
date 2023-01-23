7dac27fa4d9cbe9f222387260b5e6f0a60a6a8cb
Sylvain Perez <sylvainperez77@gmail.com>
2023-01-23 16:11:07 +0100

    add comments

--- controllers/user.js
+++ controllers/user.js
@@ -24,6 +24,7 @@ exports.signup = (req, res, next) => {
         .catch(error => res.status(500).json({ error }))
 }
 
+// login to authenticate //
 exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
         .then(user => {
