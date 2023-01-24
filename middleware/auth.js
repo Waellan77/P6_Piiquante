76a0090bd801a9c81cb0174767593feb3f885d6c
Sylvain Perez <sylvainperez77@gmail.com>
2023-01-24 10:10:31 +0100

    add comments

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,15 @@
+// Import Jsonwebtoken for token auth //
 const jwt = require('jsonwebtoken')
 
 module.exports = (req, res, next) => {
     try {
+        // we split between bearer and token and we recover the token in the header //
         const token = req.headers.authorization.split(' ')[1]
-        const decodedToken = jwt.verify(token, 'TOKEN')
+
+        // we decode our token, if it is valid, we continue otherwise an error //
+        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET')
+
+        //we get the userId and we add it to the request object which is sent to the routes //
         const userId = decodedToken.userId
         req.auth = {
             userId: userId
