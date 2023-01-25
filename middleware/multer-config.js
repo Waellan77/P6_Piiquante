0b35f06a93e15ecb86d4df0e9c19fdaf6a412e2e
Sylvain Perez <sylvainperez77@gmail.com>
2023-01-25 17:19:41 +0100

    add comments

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,20 +1,29 @@
+// import multer //
 const multer = require('multer')
 
+// create a dictionary of image extensions //
 const MIME_TYPES = {
     'image/jpg': 'jpg',
     'image/jpeg': 'jpg',
     'image/png': 'png'
 }
 
+// create a configuration object for multer and use diskStorage //
 const storage = multer.diskStorage({
+    // We indicate the destination to store the images //
     destination: (req, file, callback) => {
         callback(null, 'images')
     },
+    // We indicate the file name to use //
     filename: (req, file, callback) => {
+        // use the original name and replace the spaces with underscores //
         const name = file.originalname.split('').join('_')
+        // We indicate the extension according to that of the original image //
         const extension = MIME_TYPES[file.mimetype]
+        // name + timestamp + extension //
         callback(null, name + Date.now() + '.' + extension)
     }
 })
 
+// export middleware multer //
 module.exports = multer({ storage }).single('image')
